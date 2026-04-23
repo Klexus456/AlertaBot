cfb9c78fed6f546114cd14921368ad63dd14bd62
Caleb <142183742+Klexus456@users.noreply.github.com>
2026-04-22 21:30:34 -0300

    commit test

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const CHANNEL_ID = "1496666032557785199";
+const ROLE_ID = "1496666722529443920";
+
+client.once("ready", () => {
+  console.log("Bot listo");
+
+  setInterval(() => {
+    const now = new Date();
+
+    const hora = now.getHours();
+    const minuto = now.getMinutes();
+
+    //  Bloque horario (1 AM a 6 AM)
+    //if (hora >= 1 && hora < 6) return;
+
+    //  Solo ejecuta en el minuto 48
+    if (minuto === 48) {
+      const channel = client.channels.cache.get(CHANNEL_ID);
+      if (!channel) return;
+
+      channel.send(`<@&${ROLE_ID}> Recordatorio automatico`);
+      console.log("Ping enviado");
+    }
+
+  }, 60000); // revisa cada minuto
+});
+
+client.login(process.env.TOKEN);
